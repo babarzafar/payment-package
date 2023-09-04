a2c115c1874ab04568bd178b1612138d8ea85840
Babar Ali <babar.ali1@aksa.com.pk>
2023-09-04 14:51:25 +0500

    Updated the file with some changes

--- src/Http/Controllers/PaymentController.php
+++ src/Http/Controllers/PaymentController.php
@@ -13,8 +13,6 @@ class PaymentController extends Controller
         try {
             // Initialize Paymob
             $paymob = new Paymob();
-
-            // Configure Paymob settings
             $conf = [
                 'apiKey' => config('payment-package.api_key'),
                 'secKey' => config('payment-package.secret_key'),
@@ -22,6 +20,40 @@ class PaymentController extends Controller
                 'integration_id' => config('payment-package.payment_method_integration_id'),
             ];
 
+            $cents = 100; // 100 for all countries and 1000 for Oman
+            $billing = [
+                "email" => 'email@email.com',
+                "first_name" => 'fname',
+                "last_name" => 'lname',
+                "street" => 'street',
+                "phone_number" => '1234567890',
+                "city" => 'city',
+                "country" => 'country',
+                "state" => 'state',
+                "postal_code" => '12345',
+            ];
+
+            $data = [
+                "amount" => 10 * $cents,
+                "currency" => 'EGP',
+                "payment_methods" => array(1234567),
+                // replace this id 1234567 with your integration ID(s)
+                "billing_data" => $billing,
+                "extras" => ["merchant_intention_id" => '123_' . time()],
+                "special_reference" => '123_' . time()
+            ];
+
+            $status = $paymob->createIntention($conf['secKey'], $data);
+            $countryCode = $paymob->getCountryCode($conf['secKey']);
+            $apiUrl = $paymob->getApiUrl($countryCode);
+            $cs = $status['cs'];
+
+            $to = $apiUrl . "unifiedcheckout/?publicKey=" . $conf['pubKey'] . "&clientSecret=$cs";
+            //echo "Pay using the URL $to";
+            return redirect($to);
+
+            exit;
+            // Configure Paymob settings
             // Get authentication token and integration IDs
             $authData = $paymob->authToken($conf);
 
